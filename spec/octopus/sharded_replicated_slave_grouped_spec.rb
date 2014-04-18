be9fa681466332a84cc19a78e36bbd6b1c449c40
Yusuke KUOKA <yusuke.kuoka@crowdworks.co.jp>
2014-04-18 16:05:03 +0900

    Add specs for slaves groups in sharded-replicated configurations

--- spec/octopus/sharded_replicated_slave_grouped_spec.rb
+++ spec/octopus/sharded_replicated_slave_grouped_spec.rb
@@ -6,19 +6,6 @@ describe "when the database is both sharded and replicated" do
     OctopusHelper.clean_connection_proxy()
   end
 
-  it "should not send all queries to the specified slave" do
-    pending()
-    # User.create!(:name => "Thiago")
-    #
-    # using_environment :not_entire_sharded do
-    #   Octopus.using(:russia) do
-    #     User.create!(:name => "Thiago")
-    #   end
-    # end
-    #
-    # User.count.should == 2
-  end
-
   it "should pick the shard based on current_shard when you have a sharded model" do
     Cat.create!(:name => "Thiago1")
 
@@ -32,13 +19,41 @@ describe "when the database is both sharded and replicated" do
     Octopus.stub(:env).and_return("sharded_replicated_slave_grouped")
 
     Cat.using(:russia).count.should == 2
+    # It distributes queries between two slaves in the slave group
     Cat.using(shard: :russia, slave_group: :slaves1).count.should == 0
+    Cat.using(shard: :russia, slave_group: :slaves1).count.should == 2
+    Cat.using(shard: :russia, slave_group: :slaves1).count.should == 0
+    # It distributes queries between two slaves in the slave group
+    Cat.using(shard: :russia, slave_group: :slaves2).count.should == 2
     Cat.using(shard: :russia, slave_group: :slaves2).count.should == 0
+    Cat.using(shard: :russia, slave_group: :slaves2).count.should == 2
 
     Cat.using(:europe).count.should == 0
     Cat.using(shard: :europe, slave_group: :slaves1)
       .count.should == 0
     Cat.using(shard: :europe, slave_group: :slaves2)
       .count.should == 2
+  end
+
+  it "should make queries to master when slave groups are configured for the shard but not selected" do
+    OctopusHelper.using_environment :sharded_replicated_slave_grouped do
+      Octopus.using(:europe) do
+        # All the queries go to :master(`octopus_shard_1`)
+
+        Cat.create!(:name => "Thiago1")
+        Cat.create!(:name => "Thiago2")
+
+        # In `database.yml` and `shards.yml`, we have configured 1 master and 6 slaves for `sharded_replicated_slave_grouped`
+        # So we can ensure Octopus is not distributing queries between them
+        # by asserting 1 + 6 = 7 queries go to :master(`octopus_shard_1`)
+        Cat.count.should == 2
+        Cat.count.should == 2
+        Cat.count.should == 2
+        Cat.count.should == 2
+        Cat.count.should == 2
+        Cat.count.should == 2
+        Cat.count.should == 2
+      end
+    end
   end
 end
